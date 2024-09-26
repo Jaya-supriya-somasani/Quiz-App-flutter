0a31a947ef6527792d6b8667c273ed954ce67aaf
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-09-26 15:24:50 +0530

    auto generated file created

--- lib/data/data_sources/remote/api_service.dart
+++ lib/data/data_sources/remote/api_service.dart
@@ -4,6 +4,7 @@ import 'package:quiz/data/data_sources/constants.dart';
 import 'package:quiz/data/models/practice_ques_model.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
+
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: baseURL)
@@ -12,12 +13,12 @@ abstract class ApiService {
 
   @GET('/explore/v3/practice/topic-test-question')
   Future<HttpResponse<List<PracticeQuesModel>>> fetchTopicExerciseQuestion({
-    @Query("pcsct_id") String? pcsctId,
-    @Query("practice_format_id") String practiceFormatId,
-    @Query("question_number") int questionNumber,
-    @Query("is_previous") bool isPrevious,
-    @Query("program_id") String programId,
-    @Query("subject_id") String subjectId,
-    @Query("chapter_id") String chapterId,
+    @Query("pcsct_id") required String pcsctId,
+    @Query("practice_format_id") required String practiceFormatId,
+    @Query("question_number") required int questionNumber,
+    @Query("is_previous") required bool isPrevious,
+    @Query("program_id") required String programId,
+    @Query("subject_id") required String subjectId,
+    @Query("chapter_id") required String chapterId,
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   width: 10,
                 ),
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     print("card clicked");
                   },
                   child: Card(
